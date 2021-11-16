9ac3bffa893b05ed10bbf812d849158a3c0e559c
Muhammet Ozturk <ozturkmuhammeet@gmail.com>
2021-11-16 21:44:42 +0300

    build added on 16/11

--- dist/js/chunk-2d207d68.58e0920a.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d207d68"],{a1df:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v(" Settings ")])},c=[],l={},s=l,a=t("2877"),i=Object(a["a"])(s,u,c,!1,null,null,null);e["default"]=i.exports}}]);
-//# sourceMappingURL=chunk-2d207d68.58e0920a.js.map
