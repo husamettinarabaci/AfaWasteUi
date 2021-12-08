8342c459e06341bfd91ddc755b5d0efe508147cb
Muhammet Ozturk <ozturkmuhammeet@gmail.com>
2021-12-08 04:25:27 +0300

    Afatek edit page added

--- src/views/afatek/customers/Edit.vue
+++ src/views/afatek/customers/Edit.vue
@@ -1,11 +1,95 @@
 <template>
     <div>
-        Afatek customers edit
+        <b-row v-if="showSelectCustomer">
+            <b-col lg="12">
+                <b-card no-body>
+                    <b-card-body>
+                        <b-form-select
+                        v-model="selectedCustomer"
+                        :options="customerList"
+                        />
+                    </b-card-body>
+                </b-card>
+            </b-col>
+        </b-row>
+        <b-row class="match-height">
+            <b-col lg="3">
+                <b-card no-body>
+                    <b-card-body>
+                        TAGS
+                    </b-card-body>
+                </b-card>
+            </b-col>
+            <b-col lg="3">
+                <b-card no-body>
+                    <b-card-body>
+                        RFID
+                    </b-card-body>
+                </b-card>
+            </b-col>
+            <b-col lg="3">
+                <b-card no-body>
+                    <b-card-body>
+                        ULT
+                    </b-card-body>
+                </b-card>
+            </b-col>
+            <b-col lg="3">
+                <b-card no-body>
+                    <b-card-body>
+                        RECY
+                    </b-card-body>
+                </b-card>
+            </b-col>
+        </b-row>
     </div>
 </template>
 
 <script>
+import { BCard, BCardBody, BRow, BCol, BFormSelect } from 'bootstrap-vue';
+import AfatekApi from '@/services/afatekapi.service';
+
 export default {
+    components: {
+        BCard,
+        BCardBody,
+        BRow,
+        BCol,
+        BFormSelect
+    },
+
+    data(){
+        return {
+            showSelectCustomer: true,
+            selectedCustomer: null,
+            customerList: [],
+            customerList: []
+        }
+    },
+
+    watch: {
+        'selectedCustomer': function(newVal){
+            console.log('customer changed to: ', newVal)
+        }
+    },
+
+    mounted(){
+        let customer = this.$route.params.customer;
+        if (customer){
+            this.showSelectCustomer = false;
+        }
+        else {
+            AfatekApi.getCustomers().then(response => {
+                this.customerList = [{ value: null, text: 'Please select some item' }]
+                this.customerList.concat(Object.values(response.Customers).map(customer => {
+                    return {
+                        value: customer.CustomerId,
+                        text: customer.CustomerName
+                    }
+                }))
+            })
+        }
+    }
 
 }
 </script>
--- src/views/afatek/customers/List.vue
+++ src/views/afatek/customers/List.vue
@@ -1,98 +1,104 @@
 <template>
-    <b-row>
-        <b-col lg="12">
-            <b-table
-            :items="customers"
-            :current-page="table.currentPage"
-            :per-page="table.perPage"
-            responsive
-            select-mode="single"
-            :fields="table.fields"
-            primary-key="id"
-            :sort-by.sync="table.sortBy"
-            show-empty
-            empty-text="No matching records found"
-            :sort-desc.sync="table.isSortDirDesc"
-            class="position-relative"
-            >
-                <!-- Column: Customer Name -->
-                <template #cell(CustomerName)="data">
-                    <span v-if="table.searchQuery.length >= 3" v-html="$options.filters.highlight(data.value, table.searchQuery)"></span>
-                    <span v-else>{{ data.value }}</span>
-                </template>
-
-                <!-- Column: Customer Link -->
-                <template #cell(CustomerLink)="data">
-                    <b-badge pill variant="light-primary" :title="data.value">
-                        {{ $moment(data.value).format('HH:mm:ss') }}
-                    </b-badge>
-                </template>
-
-                <!-- Column: Apps -->
-                <template #cell(Apps)="data">
-                    <b-badge variant="light-primary" v-if="data.items[Enums.WEB_APP_TYPE_RFID] === Enums.STATU_ACTIVE">RFID</b-badge>
-                    <b-badge variant="light-info" v-if="data.items[Enums.WEB_APP_TYPE_ULT] === Enums.STATU_ACTIVE">ULT</b-badge>
-                    <b-badge variant="light-warning" v-if="data.items[Enums.WEB_APP_TYPE_RECY] === Enums.STATU_ACTIVE">RECY</b-badge>
-                </template>
-
-                <template #cell(actions)>
-                    Actions
-                </template>
-            </b-table>
-            <div class="mx-2 mb-2">
-                <b-row>
-                    <b-col
-                    cols="12"
-                    sm="6"
-                    class="d-flex align-items-center justify-content-center justify-content-sm-start"
+    <b-card no-body>
+        <b-card-body>
+            <b-row>
+                <b-col lg="12">
+                    <b-table
+                    :items="customers"
+                    :current-page="table.currentPage"
+                    :per-page="table.perPage"
+                    responsive
+                    select-mode="single"
+                    :fields="table.fields"
+                    primary-key="id"
+                    :sort-by.sync="table.sortBy"
+                    show-empty
+                    empty-text="No matching records found"
+                    :sort-desc.sync="table.isSortDirDesc"
+                    class="position-relative"
                     >
-                    <span class="text-muted"></span>
-                    </b-col>
-                    <!-- Pagination -->
-                    <b-col
-                    cols="12"
-                    sm="6"
-                    class="d-flex align-items-center justify-content-center justify-content-sm-end"
-                    >
-
-                    <b-pagination
-                        v-model="table.currentPage"
-                        :total-rows="table.totalItems"
-                        :per-page="table.perPage"
-                        first-number
-                        last-number
-                        class="mb-0 mt-1 mt-sm-0"
-                        prev-class="prev-item"
-                        next-class="next-item"
-                    >
-                        <template #prev-text>
-                        <feather-icon
-                            icon="ChevronLeftIcon"
-                            size="18"
-                        />
+                        <!-- Column: Customer Name -->
+                        <template #cell(CustomerName)="data">
+                            <span v-if="table.searchQuery.length >= 3" v-html="$options.filters.highlight(data.value, table.searchQuery)"></span>
+                            <span v-else>{{ data.value }}</span>
                         </template>
-                        <template #next-text>
-                        <feather-icon
-                            icon="ChevronRightIcon"
-                            size="18"
-                        />
+
+                        <!-- Column: Customer Link -->
+                        <template #cell(CustomerLink)="data">
+                            <b-badge pill variant="light-primary" :title="data.value">
+                                {{ $moment(data.value).format('HH:mm:ss') }}
+                            </b-badge>
                         </template>
-                    </b-pagination>
 
-                    </b-col>
+                        <!-- Column: Apps -->
+                        <template #cell(Apps)="data">
+                            <b-badge variant="light-primary" v-if="data.items[Enums.WEB_APP_TYPE_RFID] === Enums.STATU_ACTIVE">RFID</b-badge>
+                            <b-badge variant="light-info" v-if="data.items[Enums.WEB_APP_TYPE_ULT] === Enums.STATU_ACTIVE">ULT</b-badge>
+                            <b-badge variant="light-warning" v-if="data.items[Enums.WEB_APP_TYPE_RECY] === Enums.STATU_ACTIVE">RECY</b-badge>
+                        </template>
 
-                </b-row>
-            </div>
-        </b-col>
-    </b-row>
+                        <template #cell(actions)>
+                            Actions
+                        </template>
+                    </b-table>
+                    <div class="mx-2 mb-2">
+                        <b-row>
+                            <b-col
+                            cols="12"
+                            sm="6"
+                            class="d-flex align-items-center justify-content-center justify-content-sm-start"
+                            >
+                            <span class="text-muted"></span>
+                            </b-col>
+                            <!-- Pagination -->
+                            <b-col
+                            cols="12"
+                            sm="6"
+                            class="d-flex align-items-center justify-content-center justify-content-sm-end"
+                            >
+
+                            <b-pagination
+                                v-model="table.currentPage"
+                                :total-rows="table.totalItems"
+                                :per-page="table.perPage"
+                                first-number
+                                last-number
+                                class="mb-0 mt-1 mt-sm-0"
+                                prev-class="prev-item"
+                                next-class="next-item"
+                            >
+                                <template #prev-text>
+                                <feather-icon
+                                    icon="ChevronLeftIcon"
+                                    size="18"
+                                />
+                                </template>
+                                <template #next-text>
+                                <feather-icon
+                                    icon="ChevronRightIcon"
+                                    size="18"
+                                />
+                                </template>
+                            </b-pagination>
+
+                            </b-col>
+
+                        </b-row>
+                    </div>
+                </b-col>
+            </b-row>
+        </b-card-body>
+    </b-card>
 </template>
 
 <script>
-import { BRow, BCol, BTable, BPagination} from 'bootstrap-vue';
+import { BCard, BCardBody, BRow, BCol, BTable, BPagination} from 'bootstrap-vue';
 import AfatekApi from '@/services/afatekapi.service';
 
 export default {
     components: {
+        BCard,
+        BCardBody,
         BRow,
         BCol,
         BTable,
