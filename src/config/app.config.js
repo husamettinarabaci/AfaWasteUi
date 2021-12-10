33531d9723490000a0ff4ac5fba426130724eb07
Muhammet Ozturk <ozturkmuhammeet@gmail.com>
2021-12-10 19:41:39 +0300

    updates

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    apiUrl: 'http://afatek.aws.afatek.com.tr',
-    socketUrl: 'ws://afatek.aws.afatek.com.tr/websocket/socket',
+    apiUrl: '',
+    socketUrl: '/websocket/socket',
     s3Url: 'https://afatek-waste-videos-s3.s3.eu-central-1.amazonaws.com/WAIT_CAM/',
     geocodingApi: `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.VUE_APP_GEOCODING_API}`,
     webApiUrl: '/webapi',
